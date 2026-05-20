3a9cb2693c0a1f628e442d26b0b8cd52e98cb423
Bades <39216599+MrBades@users.noreply.github.com>
2026-05-20 20:18:03 +0100

    Add database migration to build process
    Added database migration step before collecting static files.

--- build_files.sh
+++ build_files.sh
@@ -5,11 +5,14 @@ set -o errexit
 echo "Installing requirements..."
 python3.12 -m pip install -r requirements.txt
 
+echo "Running database migrations..."
+# This will build the core_invoice table in your Vercel Postgres DB
+python3.12 manage.py migrate --noinput
+
 echo "Collecting static files..."
 python3.12 manage.py collectstatic --noinput --clear
 
-# Note: Vercel static build expects files in staticfiles_build (configured in vercel.json)
-# We move them there to be sure
+# Vercel static build expects files in staticfiles_build (configured in vercel.json)
 mkdir -p staticfiles_build
 cp -r staticfiles/* staticfiles_build/
 
